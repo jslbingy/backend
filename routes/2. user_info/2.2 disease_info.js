0a88e2b84aec655f07e226e85b60ed41841ba3ec
Bingoy <jslbingy@gmail.com>
2020-01-16 00:51:03 +0800

    add

--- routes/2. user_info/2.2 disease_info.js
+++ routes/2. user_info/2.2 disease_info.js
@@ -32,37 +32,4 @@ function buildDao(d, user) {
     return result;
 }
 
-// /**
-//  * API 2.1.2 update user personal info
-//  */
-// router.put(`/`, async function (req, res, next) {
-//     let con;
-//     try {
-//         con = await connection(dbConfig);
-//         let body = { ...req.body };
-//         let user = { ...req.user };
-//         let update = await query(con, `update user set ? where id = ?`, [body, user.id]);
-//         res.json(rb.build({}, `user info updated`));
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-// /**
-//  * API 2.1.3 get user personal info
-//  */
-// router.get(`/`, async function (req, res, next) {
-//     let con;
-//     try {
-//         con = await connection(dbConfig);
-//         let user = { ...req.user };
-//         let result = await query(con, `select * from user where id = ?`, [user.id]);
-//         result = result[0];
-//         delete result, password;
-//         res.json(rb.build(result, `retrieve user info successfully`));
-//     } catch (err) {
-//         next(err);
-//     }
-// })
-
 module.exports = router;
