32e84a0dec2c54bf54306753796c90416085a779
Bingoy <jslbingy@gmail.com>
2020-01-20 00:05:59 +0800

    add

--- routes/2. user_info/2.1 general_info.js
+++ routes/2. user_info/2.1 general_info.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const router = express.Router();
+const moment = require(`moment`);
 const dbConfig = require(`../../dbConfig`);
 const connection = require(`../../helpers/connection`);
 const query = require(`../../helpers/query`);
@@ -14,6 +15,10 @@ router.post(`/`, async function (req, res, next) {
         con = await connection(dbConfig);
         let body = { ...req.body };
         let user = { ...req.user };
+        let birth_year = moment(body.dob).format('YYYY');
+        let current_year = moment().year();
+        body.age = current_year - birth_year;
+        delete body.dob;
         let update = await query(con, `update user set ? where id = ?`, [body, user.id]);
         res.json(rb.build({}, `user info saved`));
     } catch (err) {
@@ -30,6 +35,10 @@ router.put(`/`, async function (req, res, next) {
         con = await connection(dbConfig);
         let body = { ...req.body };
         let user = { ...req.user };
+        let birth_year = moment(body.dob).format('YYYY');
+        let current_year = moment().year();
+        body.age = current_year - birth_year;
+        delete body.dob;
         let update = await query(con, `update user set ? where id = ?`, [body, user.id]);
         res.json(rb.build({}, `user info updated`));
     } catch (err) {
@@ -47,7 +56,8 @@ router.get(`/`, async function (req, res, next) {
         let user = { ...req.user };
         let result = await query(con, `select * from user where id = ?`, [user.id]);
         result = result[0];
-        delete result, password;
+        result.gender = result.gender ? 'male' : 'female';
+        delete result.password;
         res.json(rb.build(result, `retrieve user info successfully`));
     } catch (err) {
         next(err);
